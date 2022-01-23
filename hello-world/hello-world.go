7094f5acda86c3f779bbbbc56ce6f2db6b0ff3cf
zerobits01 <zerobits0101@gmail.com>
2022-01-23 23:45:51 +0330

    initializing the project with hello-world

---
+++ hello-world/hello-world.go
@@ -0,0 +1,23 @@
+// defining package like java and C#
+package main
+
+// importing libraries, fmt for input output
+import "fmt"
+
+
+/*
+go run filename.go # run the file
+go build filename.go # build the app and outputs a binary file to run
+*/
+
+
+// func for defining new function and package name and function name which should be main and we have main function like C-based
+func main() {
+    fmt.Println("hello world")
+}
+
+
+
+// this language has its new paradigm
+// its not OOP but its not-not-OOP
+// come to learn with golang new mindset!!!!
